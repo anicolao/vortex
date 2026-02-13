d27872d66b46d537ae72eddb1a61f9a76846b1c2
anicolao <anicolao@users.noreply.github.com>
2026-02-13 02:19:38 +0000

    deploy: 19049f6e5a5e3bb712707ceb044e5ca5176227ec

---
+++ pr3/_app/immutable/nodes/5.BhZU6HBz.js
@@ -0,0 +1,44 @@
+import{c as g,a as h,f as x}from"../chunks/BHnUeLfg.js";import{i as b}from"../chunks/BZyZ3sR9.js";import{f as _,O as $,Q as z,T as c,R as p,U as d}from"../chunks/mra9ftPF.js";import{e as m}from"../chunks/D1USUX2b.js";import{g as y,G as M,s as w}from"../chunks/Dktvg-K7.js";import{z as H}from"../chunks/C5iOoRCJ.js";import{g as v}from"../chunks/DQHeCYGR.js";import{b as I,c as T}from"../chunks/B2R-KUoo.js";import{I as U,s as G}from"../chunks/Db2YuAzI.js";import{l as L,s as N}from"../chunks/C7hNub10.js";function P(r,o){const i=L(o,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.563.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2026 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2026.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2026 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const n=[["path",{d:"M21 4H3"}],["path",{d:"M18 8H6"}],["path",{d:"M19 12H9"}],["path",{d:"M16 16h-6"}],["path",{d:"M11 20H9"}]];U(r,N({name:"tornado"},()=>i,{get iconNode(){return n},children:(t,s)=>{var a=g(),e=_(a);G(e,o,"default",{}),h(t,a)},$$slots:{default:!0}}))}var S=x('<div class="login-container svelte-1x05zx6"><div class="vortex-icon svelte-1x05zx6"><!></div> <h1 class="svelte-1x05zx6">The Vortex</h1> <p class="svelte-1x05zx6">Enter the flow. Split costs. Stay balanced.</p> <button class="btn-primary svelte-1x05zx6">Sign in with Google</button> <button class="btn-dev svelte-1x05zx6" data-testid="dev-login">Dev Login</button></div>');function q(r,o){$(o,!1);async function i(){try{const l=y(H),u=new M;await w(l,u),v("/dashboard")}catch(l){console.error("Error signing in",l)}}function n(){I.dispatch(T({uid:"test-user-id",displayName:"Test User",email:"test@example.com",photoURL:null})),v("/dashboard")}b();var t=S(),s=c(t),a=c(s);P(a,{size:80,color:"var(--color-primary)"}),d(s);var e=p(s,6),f=p(e,2);d(t),m("click",e,i),m("click",f,n),h(r,t),z()}export{q as component};
